79d515c95b4689bc8bc9141a0ea92ff41b23064a
David Eriksson <david.eriksson@ehalsomyndigheten.se>
2016-01-13 12:49:36 +0100

    Panictest and integrated module with unit test

--- src/module.rs
+++ src/module.rs
@@ -2,3 +2,10 @@ pub fn yo(string: String) -> String {
     let result = string + " OK";
     return result;
 }
+
+#[test]
+fn first_test() {
+    let result = "Hello";
+    assert_eq!("Hello", result);
+}
+
--- tests/moduletests.rs
+++ tests/moduletests.rs
@@ -2,6 +2,12 @@ extern crate module;
 
 #[test]
 fn first_test() {
-    let result = "Hello";
-    assert_eq!("Hello", result);
+    let result = module::yo("Hello".to_string());
+    assert_eq!("Hello OK", result);
+}
+
+#[test]
+#[should_panic(expected = "assertion failed")]
+fn should_panic() {
+    assert_eq!("1", "2");
 }
